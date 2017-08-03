a95a382ecefd98ac5f0866211fc309ec99fd0f26
luismcl <yo@luismiguelcisneros.com>
2017-08-03 11:11:11 +0200

    Add Icons

--- app/views/buildmap.scala.js
+++ app/views/buildmap.scala.js
@@ -6,6 +6,34 @@ function initMap() {
     console.log("Build Map")
     var labels = ["F","C","3","2","1","HC"];
 
+    var icons = {
+      5: {
+        icon: {
+              url:"@routes.Assets.versioned("images/hctour.png")",
+              scaledSize: new google.maps.Size(20, 20)
+              }
+      },
+      4: {
+        icon: {
+              url:"@routes.Assets.versioned("images/1tour.png")",
+              scaledSize: new google.maps.Size(20, 20)
+              }
+      },
+      3: {
+        icon: {
+               url:"@routes.Assets.versioned("images/2tour.png")",
+               scaledSize: new google.maps.Size(20, 20)
+               }
+      },
+      2: {
+        icon: {
+            url:"@routes.Assets.versioned("images/3tour.png")",
+            scaledSize: new google.maps.Size(20, 20)
+        }
+      }
+    };
+
+
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8
   });
@@ -25,7 +53,8 @@ function initMap() {
         position: {lat: @se.segment.end.coordinates(0), lng:  @se.segment.end.coordinates(1)},
         map: map,
         title: '@se.segment.name',
-        label: labels[@se.segment.climbCategory]
+        icon: icons[@se.segment.climbCategory].icon,
+        scale:10
       });
 
        marker_@{se._id}.addListener('click', function(){
